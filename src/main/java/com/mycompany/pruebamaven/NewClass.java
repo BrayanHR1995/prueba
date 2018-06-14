e35251702018f288628371a6458e9c12a861991d
Brayan <hrbrayan1995@gmail.com>
2018-06-14 13:03:02 -0500

    prueba1

---
+++ src/main/java/com/mycompany/pruebamaven/NewClass.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pruebamaven;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author BrayanHR09
+ */
+public class NewClass {
+    
+    
+    
+        public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner teclado=new Scanner(System.in);
+        String frase;
+        System.out.println("ingrese una frase");
+        frase=teclado.nextLine();
+        System.out.println((frase));
+
+
+    }
+}
